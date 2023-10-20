76fd5938647d48c2873d98811043f8c91d790b4e
Alfredo-Azamar <a01798100@tec.mx>
2023-10-20 14:57:47 -0600

    Final changes

--- app/src/main/java/mx/grm/prototipo4/ui/home/HomeFragment.kt
+++ app/src/main/java/mx/grm/prototipo4/ui/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -81,29 +82,32 @@ class HomeFragment : Fragment() {
     private fun uploadCustomer() {
         binding.btnUploadCostumer.setOnClickListener {
             val name = binding.etName.text.toString()
-            val p_lastName = binding.etPLastName.text.toString()
-            val m_lastName = binding.etMLastName.text.toString()
+            val pLastName = binding.etPLastName.text.toString()
+            val mLastName = binding.etMLastName.text.toString()
             val curp = binding.etCurp.text.toString()
             val bDate = binding.etBDate.text.toString()
             val gender = binding.spGender.selectedItem.toString()
             val vulSituation:Array<String> = getCond()
 
-            viewModel.uploadCostumer(name, p_lastName, m_lastName, curp, bDate, gender, vulSituation) {success ->
-                if (success) {
-                    val token = viewModel.customerToken.value
-                    if (!token.isNullOrBlank()){
-                        val alert = AlertDialog.Builder(requireActivity())
-                            .setTitle("T O K E N")
-                            .setMessage("Se le proporciona el siguiente token: ${token}")
-                            .setCancelable(false)
-                            .setPositiveButton("Aceptar"){_, _ ->
-                                cleanRegistration()
-                            }
-                        alert.show()
-                    } else {
-                        println("ERROR")
+
+            if (curp != ""){
+                viewModel.uploadCostumer(name, pLastName, mLastName, curp, bDate, gender, vulSituation) {success ->
+                    if (success) {
+                        val token = viewModel.customerToken.value
+                        if (!token.isNullOrBlank()){
+                            val alert = AlertDialog.Builder(requireActivity())
+                                .setTitle("T O K E N")
+                                .setMessage("Se le proporciona el siguiente token: ${token}")
+                                .setCancelable(false)
+                                .setPositiveButton("Aceptar"){_, _ ->
+                                    cleanRegistration()
+                                }
+                            alert.show()
+                        }
                     }
                 }
+            } else {
+                Toast.makeText(requireActivity(), "Llena los campos", Toast.LENGTH_SHORT).show()
             }
         }
     }
@@ -183,7 +187,7 @@ class HomeFragment : Fragment() {
 
     private fun getNoticePriv() {
         binding.tvNoticePriv.setOnClickListener {
-            val url = "https://github.com/Alfredo-Azamar/PruebaPlatoPaTodos/blob/main/Aviso%20de%20privacidad.pdf"
+            val url = "https://platopatodose3.ddns.net:8080/Aviso_de_privacidad.html"
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
             startActivity(intent)
         }
